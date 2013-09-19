987ab057670f9f431815e845b1be8b067902a84d
abhishek <abhishek.kumar.cse08@itbhu.ac.in>
2013-09-19 21:36:55 +0530

    Changed

--- welcome/error.html.php
+++ welcome/error.html.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
-<head>
-	<title>Error!</title>
-	<meta charset="utf-8">
-</head>
-<body>
-	<?php
-		echo $error;
-	?>
-</body>
+<html>
+	<head>
+		<title>Error!</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<?php
+			echo $error;
+		?>
+	</body>
+</html>
